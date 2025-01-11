72cefca6ce88200f368816655be5c36893f72183
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-01-11 15:08:06 +0000

    Add infinity check and bug fixes

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let a = "";
 let b = "";
 let operator = "";
 let usedDecimal = false;
+let pressedEqual = false;
 
+// Function that resets everything on the webpage to 0
 function reset() {
 	a = "";
 	b = "";
@@ -19,6 +21,7 @@ function reset() {
 
 document.addEventListener("onload", reset());
 
+// Function that returns answer by operating on the two operands
 function operate(a, b, operator) {
 	let answer;
 	if (operator === "+") {
@@ -34,16 +37,54 @@ function operate(a, b, operator) {
 	return Math.round(answer * 100) / 100;
 }
 
+function getNumberFromId(numberButton) {
+	let value = numberButton.id;
+	switch (value) {
+		case "one-button":
+			return "1";
+		case "two-button":
+			return "2";
+		case "three-button":
+			return "3";
+		case "four-button":
+			return "4";
+		case "five-button":
+			return "5";
+		case "six-button":
+			return "6";
+		case "seven-button":
+			return "7";
+		case "eight-button":
+			return "8";
+		case "nine-button":
+			return "9";
+		case "zero-button":
+			return "0";
+		case "decimal-button":
+			return ".";
+	}
+}
+
+// This part of code reads numbers and stores them in variables (a or b)
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((numberButton) => {
 	numberButton.addEventListener("click", () => {
-		let temp = numberButton.textContent;
+		let temp = getNumberFromId(numberButton);
+		console.log({ temp });
+
+		if (pressedEqual) {
+			reset();
+			pressedEqual = false;
+		}
+
+		// Check to ensure that decimal is used once for a or b
 		if (temp === "." && usedDecimal === false) {
 			usedDecimal = true;
 		} else if (temp === "." && usedDecimal === true) {
 			return;
 		}
-		console.log({ b });
+
+		// Check to ensure either a or b is updated depending on operator is present or not
 		if (operator === "") {
 			a += temp;
 			resultDisplay.textContent = a;
@@ -94,11 +135,19 @@ const equalButton = document.querySelector("#equal-button");
 equalButton.addEventListener("click", () => {
 	if (a != "" && b != "") {
 		let ans = operate(a, b, operator);
-		resultDisplay.textContent = ans;
-		calculationDisplay.textContent = a + operator + b;
-		a = ans;
-		b = "";
+		if (ans === Infinity) {
+			resultDisplay.textContent = "bruh";
+			calculationDisplay.textContent = "(≖_≖)";
+			a = "";
+			b = "";
+		} else {
+			resultDisplay.textContent = ans;
+			calculationDisplay.textContent = a + operator + b;
+			a = ans;
+			b = "";
+		}
 		usedDecimal = false;
+		pressedEqual = true;
 	} else {
 		return;
 	}
